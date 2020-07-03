9f8987a56c3a64ccd22b87d9b905ac42546bed2f
Kyle Cole <kcole@aveshka.com>
2020-07-03 17:06:14 -0400

    forms added with simple validation and 404 page

--- run.py
+++ run.py
@@ -1,31 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Flask</h1>'
+    # return '<h1>Hello Flask</h1>'
+    return render_template('home.html')
 
-@app.route('/cakehole')
-def cakehole():
-    return '<h1>CAKEHOLE</h1>'
+@app.route('/users/')
+def users():
+    users = ["cakehole", "sally", "jim"]
+    return render_template("users.html", users=users)
 
-@app.route('/cakehole/<username>')
+@app.route('/users/<username>')
 def user(username):
-    return "<h1>Hello, {}</h1>".format(username.upper())
+    letters = list(username)
+    dict = {
+        "name":username,
+    }
+
+    user_logged_in = True
+    return render_template('profile.html', 
+                user_logged_in=user_logged_in,
+                username=username, 
+                time=datetime.utcnow(),
+                letters=letters, 
+                dict=dict, 
+                )
+    # return "<h1>Hello, {}</h1>".format(username.upper())
     # return "<h1>Hello, {}</h1>".format(username[99])
 
-@app.route('/lorem/<name>')
-def lorem(name):
-    n = name
-    if n[-1] == "y":
-        output = n[0:-1] + "iful"
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.route('/thankyou')
+def thankyou():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template('thank_you.html', first=first, last=last)
+
+@app.route('/login')
+def login():
+    valid_users = ["cakehole", "sally", 'jim']
+    username = request.args.get('username')
+    if username in valid_users:
+        return render_template('profile.html', username=username, time=datetime.utcnow())
     else:
-        output = n + "y"
-    
-    return '<h1>Hi, {}!</h1>'.format(output)
+        return render_template('404.html'), 404
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
